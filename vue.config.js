fff6b0d61e2bd0ab755aac7f6a91cc118fbf2404
fullstack001 <mr.dimas332@gmail.com>
2024-02-20 21:53:09 +0200

    update new admin feature

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,8 @@ module.exports = {
     disableHostCheck: true,
     proxy: {
       "/socket.io": {
-        target: "http://127.0.0.1:5000", // Your Socket.IO server address
+        // target: "http://127.0.0.1:5000", // Your Socket.IO server address
+        target: "https://api.pdfden.com", // Your Socket.IO server address
         ws: true,
         changeOrigin: true,
       },
